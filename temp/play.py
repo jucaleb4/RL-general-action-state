4a32425b7c190b17af1a86820f23b0cb5c41621e
jucaleb4 <calebju@gmail.com>
2024-02-22 17:06:05 -0500

    updated notebook

--- temp/play.py
+++ temp/play.py
@@ -3,9 +3,9 @@ from sklearn.linear_model import SGDClassifier
 
 X = [[0, 0], [1, 1], [1, 0], [0, 1]]
 y = [0, 0, 1, 1]
-import ipdb; ipdb.set_trace()
 rbf_feature = RBFSampler(gamma=1, random_state=1)
 X_features = rbf_feature.fit_transform(X)
+import ipdb; ipdb.set_trace()
 clf = SGDClassifier(max_iter=5, tol=1e-3)
 clf.fit(X_features, y)
 clf.score(X_features, y)
