Add bootstrapping to rollout, fix normalization bugs, more tuning options, strong regularization with Shannon entropy, and mimic advantage function estimation like PPO

rl/pmd.py
@@ -27,10 +27,11 @@ class PMD(RLAlg):
         super().__init__(env, params)
         self.check_params()
 
+        # initialize
+        self._last_s, _ = env.reset()
+
         self.rollout = Rollout(env, **params)
         self.episode_rewards = []
-
-        self.initialized_env = False
 
     def _learn(self, n_iter):
         """ Runs PMD algorithm for `n_iter`s """
@@ -54,35 +55,40 @@ class PMD(RLAlg):
         # print(f"Final policy:\n{self.policy}")
         self.save_episode_reward_and_len()
 
+        # return moving average reward
+        ep_cum_rwds = self.rollout.get_episode_rewards()
+        t = min(25, len(ep_cum_rwds))
+        return np.mean(ep_cum_rwds[-t:])
+
     def check_params(self):
-        if "single_trajectory" not in self.params:
-            warnings.warn("Did not pass in 'single_trajectory' into params, defaulting to False")
-            self.params["single_trajectory"] = False
         if "rollout_len" not in self.params:
             warnings.warn("Did not pass in 'rollout_len' into params, defaulting to 1000")
             self.params["rollout_len"] = 1000
 
     def collect_rollouts(self):
         """ Collect samples for policy evaluation """
-        self.rollout.clear_batch(keep_last_obs=self.params["single_trajectory"])
+        self.rollout.clear_batch()
 
-        if not self.initialized_env or not self.params["single_trajectory"]:
-            (s,_) = self.env.reset()
-            self.initialized_env = True
-        else:
-            s = self.rollout.get_state(0)
-
-        a = self.policy_sample(s)
-        self.rollout.add_step_data(s, a)
-
+        s = self._last_s
         for t in range(self.params["rollout_len"]): 
-            (s, r, term, trunc, _)  = self.env.step(a)
-            a = self.policy_sample(s)
-            self.rollout.add_step_data(s, a, r, term, trunc)
+            v_s = self.estimate_value(s)
 
-            if term or trunc:
-                self.env.reset()
-                break
+            a = self.policy_sample(s)
+            (next_s, r_raw, term, trunc, _)  = self.env.step(a)
+            r = self.normalize_rwd(r_raw)
+            # if truncated due to time limit, bootstrap cost-to-go
+            if trunc:
+                v_next_s = self.estimate_value(next_s)
+                r += self.params["gamma"] * v_next_s
+            if t == self.params["rollout_len"]-1:
+                self._last_pred_value = self.estimate_value(next_s)
+                self._last_state_done = done
+            done = term or trunc
+            self.rollout.add_step_data(s, a, r, done, v_s, r_raw=r_raw)
+            s = next_s
+            if done:
+                s, _ = self.env.reset()
+                self._last_s = np.copy(s)
 
     @abstractmethod
     def policy_evaluate(self):
@@ -140,11 +146,14 @@ class PMD(RLAlg):
     def normalize_rwd(self, r):
         return r
 
+    def estimate_value(self, s):
+        raise NotImplemented
+
     def save_episode_reward_and_len(self):
         rwd_arr = self.rollout.get_episode_rewards()
         len_arr = self.rollout.get_episode_lens()
 
-        if "fname" not in self.params:
+        if len(self.params.get("fname", "")) == 0:
             warnings.warn("No filename given, not saving")
             return
         fmt="%1.2f,%i"
@@ -307,8 +316,8 @@ class PMDGeneralStateFiniteAction(PMD):
 
         # uniform policy and function approximation
         self.env_warmup()
-        (q_est, sa_visited_tuple, _) = self.rollout.visited_stateaction_value()
-        (X, _) = sa_visited_tuple
+        (_, _, s_visited, _) = self.rollout.get_est_stateaction_value()
+        X = s_visited
         self.fa = FunctionApproximator(self.n_actions, X, params)
         self.last_thetas = np.zeros((self.n_actions, self.fa.dim), dtype=float)
         self.last_intercepts = np.zeros((self.n_actions, 1), dtype=float)
@@ -319,6 +328,22 @@ class PMDGeneralStateFiniteAction(PMD):
         # TODO: Remove this is doing NN or SGD
         return
 
+    def _get_policy(self, s):
+        log_policy_at_s = np.zeros(self.n_actions, dtype=float)
+        for i in range(self.n_actions):
+            self.fa.set_coef(self.theta_accum[i], i)
+            self.fa.set_intercept(self.intercept_accum[i], i)
+            log_policy_at_s[i] = self.fa.predict(np.atleast_2d(s), i)
+        # TEMP
+        c_h = self.params.get("c_h", 0)
+        log_policy_at_s *= (c_h+1)**(-self.t)
+        policy_at_s = np.exp((log_policy_at_s - np.max(log_policy_at_s)))
+        policy_at_s = np.atleast_2d(policy_at_s)
+
+        safe_normalize_row(policy_at_s)
+
+        return np.squeeze(policy_at_s)
+
     def policy_sample(self, s):
         """ Samples random action from current policy """
         if not self.updated_at_least_once:
@@ -327,21 +352,15 @@ class PMDGeneralStateFiniteAction(PMD):
         eps = self.params.get("eps_explore", 0)
         assert 0 <= eps <= 1
 
-        log_policy_at_s = np.zeros(self.n_actions, dtype=float)
-        for i in range(self.n_actions):
-            self.fa.set_coef(self.theta_accum[i], i)
-            self.fa.set_intercept(self.intercept_accum[i], i)
-            log_policy_at_s[i] = self.fa.predict(np.atleast_2d(s), i)
-        policy_at_s = np.exp((log_policy_at_s - np.max(log_policy_at_s)))
-        policy_at_s = np.atleast_2d(policy_at_s)
-
-        safe_normalize_row(policy_at_s)
-
-        pi = (1.-eps)*np.squeeze(policy_at_s) + eps/self.n_actions
+        pi = (1.-eps)*self._get_policy(s) + eps/self.n_actions
         return self.rng.choice(self.n_actions, p=pi)
 
     def policy_evaluate(self):
         """ Estimates Q function and stores in self.Q_est """
+        self.obs_runstat.update()
+        self.action_runstat.update()
+        self.rwd_runstat.update()
+
         if self.params.get("train_method", "mc") == "mc":
             self.monte_carlo_Q()
         elif self.params["train_method"] == "ctd":
@@ -357,35 +376,32 @@ class PMDGeneralStateFiniteAction(PMD):
         We update our running stats (e.g., mean and variance) for next
         iteration
         """
-        self.obs_runstat.update()
-        self.action_runstat.update()
-        self.rwd_runstat.update()
-
         # check if we are missing any points 
-        (q_est, sa_visited_tuple, ep_truncated) = self.rollout.visited_stateaction_value()
-        (s_visited, a_visited) = sa_visited_tuple
-
-        # TODO: Make this a setting? Also see if we terminated last step (unlikely right now)
-        if ep_truncated:
-            last_s, last_a = s_visited[-1], a_visited[-1].flat[0]
-            self.fa.set_coef(self.last_thetas[last_a], last_a)
-            self.fa.set_intercept(self.last_intercepts[last_a], last_a)
-            q_last_sa_est = self.fa.predict(np.atleast_2d(last_s), last_a)
-            gamma_weight = np.power(self.params["gamma"], np.arange(1,1+len(q_est)))[::-1]
-            q_est += q_last_sa_est * gamma_weight
+        (q_est, adv_est, s_visited, a_visited) = self.rollout.get_est_stateaction_value(
+            self._last_pred_value,
+            self._last_state_done,
+        )
 
         X = s_visited
-        y = q_est
+        y = adv_est if self.params["use_advantage"] else q_est
+
+        # print(f">>> last pred val: {self._last_pred_value} | done: {self._last_state_done}")
+        # print(f"Rewards variance: {self.rwd_runstat.var}")
+        # print(f"Some y's: {y[:10]} and {y[-10:]}")
 
         # extract fitted parameters
+        not_visited_actions = []
         for i in range(self.n_actions):
             action_i_idx = np.where(a_visited==i)[0]
             if len(action_i_idx) == 0:
-                print(f"Did not update action {i}")
+                not_visited_actions.append(i)
                 continue
             self.fa.update(X[action_i_idx], y[action_i_idx], i)
             self.last_thetas[i] = np.copy(self.fa.get_coef(i))
             self.last_intercepts[i] = np.copy(self.fa.get_intercept(i))
+        if len(not_visited_actions) > 0:
+            print(f"Did not update actions {not_visited_actions}")
+        # print(f"mag={np.max(np.abs(self.last_thetas))}, mean={np.mean(self.last_thetas)}, std={np.std(self.last_thetas)}")
 
     def ctd_Q(self):
         raise NotImplemented
@@ -404,8 +420,14 @@ class PMDGeneralStateFiniteAction(PMD):
     def kl_policy_update(self):
         """ Policy update with PMD and KL divergence """
         self.updated_at_least_once = True
-        self.theta_accum += self.get_stepsize_schedule()*self.last_thetas
-        self.intercept_accum += self.get_stepsize_schedule()*self.last_intercepts
+        # TEMP
+        # self.theta_accum += self.get_stepsize_schedule()*self.last_thetas
+        # self.intercept_accum += self.get_stepsize_schedule()*self.last_intercepts
+        c_h = self.params.get("c_h", 0)
+        alpha = (c_h + self.get_stepsize_schedule()**(-1))**-1
+        alpha *= (c_h+1)**self.t
+        self.theta_accum += alpha * self.last_thetas
+        self.intercept_accum += alpha * self.last_intercepts
 
     def tsallis_policy_update(self):
         """ Policy update with PMD and Tsallis divergence (with p=1/2) """
@@ -441,8 +463,8 @@ class PMDGeneralStateFiniteAction(PMD):
         self.rollout_len = old_rollout_len
 
         # TODO: Can we delete these?
-        self.obs_runstat.update()
-        self.action_runstat.update()
+        # self.obs_runstat.update()
+        # self.action_runstat.update()
         self.rwd_runstat.update()
 
         print("Finished normalization warmup")
@@ -454,14 +476,28 @@ class PMDGeneralStateFiniteAction(PMD):
         return s
 
     def normalize_action(self, a):
-        self.obs_runstat.push(a)
+        self.action_runstat.push(a)
         if self.params.get("normalize_action", False):
             return np.divide(a-self.action_runstat.mean, np.sqrt(self.action_runstat.var))
         return a
 
     def normalize_rwd(self, r):
+        """ Only scale reward, do not re-center """
+        self.rwd_runstat.push(r)
         if self.params.get("normalize_rwd", False):
-            return np.divide(r-self.rwd_runstat.mean, np.sqrt(self.rwd_runstat.var**0.5))
+            return np.divide(r, np.sqrt(self.rwd_runstat.var))
+            # return np.divide(r-self.rwd_runstat.mean, np.sqrt(self.rwd_runstat.var**0.5))
         return r
+
+    def estimate_value(self, s):
+        if not self.updated_at_least_once:
+            return 0
+        q_s = []
+        for i in range(self.n_actions):
+            self.fa.set_coef(self.last_thetas[i], i)
+            self.fa.set_intercept(self.last_intercepts[i], i)
+            q_s.append(self.fa.predict(np.atleast_2d(s), i))
+        return np.dot(q_s, self._get_policy(s))
+
 
 # class PMDGeneralStateAction(PMD):

rl/rollout.py
@@ -64,20 +64,23 @@ class Rollout:
 
         # Create arrays to store the batch information (row by row)
         capacity = 32
-        self.iter_ct = 0
-        self.reset_iter_ct = 1 # first step is a reset step
+        self.time_ct = 0
+        self.reset_time_ct = 1 # first step is a reset step
         self.s_batch = np.zeros((capacity,) + obs_dim, dtype=obs_type)  
         self.a_batch = np.zeros((capacity,) + action_dim, dtype=action_type)
-
         self.r_batch = np.zeros(capacity, dtype=float)
-        self.terminate_batch = np.zeros(capacity, dtype=bool)
-        self.truncate_batch = np.zeros(capacity, dtype=bool)
+        self.v_batch = np.copy(self.r_batch)
+        self.done_batch = np.zeros(capacity, dtype=bool)
         self.reset_steps = np.zeros(capacity, dtype=int)
 
-        self.all_episode_reward = []
-        self.all_episode_len = []
-        self.curr_episode_time = 0
-        self.curr_episode_cum_rwd = 0
+        self.s_raw_batch = np.copy(self.s_batch)
+        self.a_raw_batch = np.copy(self.a_batch)
+        self.r_raw_batch = np.copy(self.r_batch)
+
+        self.all_ep_cum_rwd = []
+        self.all_ep_len = []
+        self.curr_ep_len = 0
+        self.curr_ep_cum_rwd = 0
 
     def set_gamma(self, gamma):
         self.gamma = gamma
@@ -86,9 +89,9 @@ class Rollout:
             self, 
             state, 
             action, 
-            reward=0, 
-            terminate=False, 
-            truncate=False,
+            reward, 
+            done,
+            value=0,
             s_raw=None,
             a_raw=None,     
             r_raw=None,
@@ -108,67 +111,68 @@ class Rollout:
             f"type(action)={native_type(action)} does not match {native_type(self.a_batch[0])}"
         assert native_type(reward) in [int, float], \
             f"type(reward)={native_type(reward)} not numerical"
-        assert native_type(terminate) == bool, \
-            f"type(terminate)={native_type(terminate)} not bool"
-        assert native_type(truncate) == bool, \
-            f"type(truncate)={native_type(truncate)} not bool"
+        assert native_type(done) == bool, \
+            f"type(done)={native_type(done)} not bool"
 
-        # TODO: Redo this so that iter_ct is not incremented until the end
-        # TODO: Change for reset to truncate...
-        self.s_batch[self.iter_ct] = state
-        self.a_batch[self.iter_ct] = action
-        self.r_batch[self.iter_ct] = reward
-        self.terminate_batch[self.iter_ct] = terminate
-        self.truncate_batch[self.iter_ct] = truncate
-        self.iter_ct += 1
+        s_raw = state if s_raw is None else s_raw
+        a_raw = action if a_raw is None else a_raw
+        r_raw = reward if r_raw is None else r_raw
 
-        if self.curr_episode_time > 0:
-            self.curr_episode_cum_rwd += reward
-        self.curr_episode_time += 1
+        self.s_batch[self.time_ct] = state
+        self.a_batch[self.time_ct] = action
+        self.r_batch[self.time_ct] = reward
+        self.done_batch[self.time_ct] = done
+        self.v_batch[self.time_ct]= value
+        self.s_raw_batch[self.time_ct] = s_raw
+        self.a_raw_batch[self.time_ct] = a_raw
+        self.r_raw_batch[self.time_ct] = r_raw
+        self.time_ct += 1
 
-        if terminate or truncate:
-            self.curr_episode_time = 0
-            self.all_episode_reward.append(self.curr_episode_cum_rwd)
-            self.all_episode_len.append(self.iter_ct)
-            self.curr_episode_cum_rwd = 0
-            print(f"Ep {len(self.all_episode_reward)} reward: {self.all_episode_reward[-1]:.2f}")
+        # self.curr_ep_cum_rwd = r_raw + self.gamma*self.curr_ep_cum_rwd
+        self.curr_ep_cum_rwd = r_raw + self.curr_ep_cum_rwd
+        self.curr_ep_len += 1
 
-        if self.iter_ct == len(self.s_batch):
+        if done:
+            moving_avg = 0
+            if len(self.all_ep_cum_rwd) > 0:
+                i = min(25, len(self.all_ep_cum_rwd))
+                moving_avg = np.mean(self.all_ep_cum_rwd[-i:])
+            print(f"Ep {len(self.all_ep_cum_rwd)} reward: {self.curr_ep_cum_rwd:.2f} (25-moving avg: {moving_avg:.2f})")
+            self.all_ep_cum_rwd.append(self.curr_ep_cum_rwd)
+            self.all_ep_len.append(self.curr_ep_len)
+            self.curr_ep_cum_rwd = 0
+            self.curr_ep_len = 0
+
+        if self.time_ct == len(self.s_batch):
             self.s_batch = add_rows_array(self.s_batch)
             self.a_batch = add_rows_array(self.a_batch)
             self.r_batch = add_rows_array(self.r_batch)
-            self.terminate_batch = add_rows_array(self.terminate_batch)
-            self.truncate_batch = add_rows_array(self.truncate_batch)
+            self.v_batch = add_rows_array(self.v_batch)
+            self.done_batch = add_rows_array(self.done_batch)
+            self.s_raw_batch = add_rows_array(self.s_raw_batch)
+            self.a_raw_batch = add_rows_array(self.a_raw_batch)
+            self.r_raw_batch = add_rows_array(self.r_raw_batch)
 
-        if terminate or truncate:
-            self.reset_steps[self.reset_iter_ct] = self.iter_ct
-            self.reset_iter_ct += 1
-            if self.reset_iter_ct == len(self.reset_steps):
-                self.reset_steps = add_rows_array(self.reset_steps)
-
-    def clear_batch(self, keep_last_obs=False):
-        """ 
-        Artifically clears the batch by resetting iteration counter. 
-        """
-        if keep_last_obs and self.iter_ct > 0:
-            self.s_batch[0] = self.s_batch[self.iter_ct-1]
-        self.iter_ct = 0
-        self.reset_iter_ct = 0
-
+    def clear_batch(self):
+        self.time_ct = 0
         self.s_batch[:] = 0
         self.a_batch[:] = 0
         self.r_batch[:] = 0
-        self.reset_steps[:] = 0
+        self.v_batch[:] = 0
+        self.done_batch[:] = 0
+        self.s_raw_batch[:] = 0
+        self.a_raw_batch[:] = 0
+        self.r_raw_batch[:] = 0
 
     def get_state(self, t):
         assert 0 <= abs(t) <= len(self.s_batch)
         return self.s_batch[t]
 
     def get_episode_rewards(self):
-        return self.all_episode_reward
+        return self.all_ep_cum_rwd
 
     def get_episode_lens(self):
-        return self.all_episode_len
+        return self.all_ep_len
 
     def compute_all_stateaction_value(self):
         """ Compute advatange function 
@@ -182,20 +186,20 @@ class Rollout:
         Q = np.zeros((n_states, n_actions), dtype=float)
         total_num_first_visits = np.zeros((n_states, n_actions), dtype=int)
 
-        if self.iter_ct == 0:
+        if self.time_ct == 0:
             warnings.warn("Have not run rollout, returning null values..")
             return (Q, total_num_first_visits > 0)
 
-        for i in range(self.reset_iter_ct+1):
+        for i in range(self.reset_time_ct+1):
             # episode duration = [episode_start, episode_end)
             if i == 0:
                 t_0 = episode_start = 0
             else:
                 t_0 = episode_start = self.reset_steps[i-1]
-            if i < self.reset_iter_ct:
+            if i < self.reset_time_ct:
                 episode_end = self.reset_steps[i]
             else:
-                episode_end = self.iter_ct
+                episode_end = self.time_ct
 
             # when two consecutive steps are termination/truncates
             if episode_start+1 >= episode_end:
@@ -236,36 +240,28 @@ class Rollout:
         Ind = total_num_first_visits > 0
         return (Q, Ind)
 
-    def visited_stateaction_value(self):
+    def get_est_stateaction_value(self, last_pred_value=0, last_state_done=False):
         """
-        :return q_est: q_estimations
-        :return sa_visited: tuple of state and action visited
+        Returns Q-function, advantage function with the corresponding
+        state-action pairs
+
+        :param last_pred_value: estimated cost-to-go for last state in rollout 
+        :param last_state_terminated: last state terminated (not truncated)
         """
-        if self.iter_ct == 0:
-            warnings.warn("Have not run rollout, returning null values..")
-            return (np.array([]), np.arary([[]]))
+        q_est = np.zeros(self.time_ct, dtype=float)
+        adv_est = np.copy(q_est)
+        # if truncated, we already appended cost-to-go
+        # if termianted, not cost-to-go
+        cum_rwd = last_pred_value * (1-int(last_state_done))
+        for t in reversed(range(self.time_ct)):
+            # if we recieved done, reset cum_rwd for new episode
+            if t < self.time_ct-1 and self.done_batch[t]:
+                cum_rwd = 0
+            cum_rwd = self.r_batch[t] + self.gamma * cum_rwd
+            q_est[t] = cum_rwd
+            adv_est[t] = cum_rwd - self.v_batch[t]
 
-        t_0 = episode_start = 0
-        episode_end = self.iter_ct # self.reset_steps[1]
-        episode_truncated = self.truncate_batch[episode_end-1]
+        s_visited = np.copy(self.s_batch[:self.time_ct])
+        a_visited = np.copy(self.a_batch[:self.time_ct])
 
-        # rewards appear on second step 
-        rwds = self.r_batch[episode_start+1:episode_end]
-        weight_factors = np.power(self.gamma, np.arange(len(rwds)))
-        weighted_rwds = np.multiply(weight_factors, rwds)
-        cumulative_weighted_rwds = np.cumsum(weighted_rwds[::-1])[::-1]
-        # normalize discount because `cumulative_weighted_rwds` weighs k-th
-        # state-action value by gamma^(k-1); we do not want this scaling
-        np.divide(cumulative_weighted_rwds, weight_factors, out=cumulative_weighted_rwds)
-
-        cutoff = min(self.cutoff, len(cumulative_weighted_rwds)-1)
-        q_est = cumulative_weighted_rwds[:-cutoff]
-        s_visited = np.copy(self.s_batch[1:len(q_est)+1])
-        a_visited = np.copy(self.a_batch[1:len(q_est)+1])
-        sa_visited = (s_visited, a_visited)
-
-        assert len(q_est) == len(s_visited) == len(a_visited), f"{len(q_est)} {len(s_visited)} {len(a_visited)}"
-
-        # print("best x-loc:", np.max(s_visited[:,0]), f"truncated: {episode_truncated}")
-
-        return (q_est, sa_visited, episode_truncated)
+        return (q_est, adv_est, s_visited, a_visited)