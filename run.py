3e276df63e0546f59b7c62cd229f6165f77355ff
Caleb GT <calebju4@gatech.edu>
2024-03-15 00:29:03 -0400

    handle perlmutter

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 import os
+import sys
 
-# just for perlmutter
-if False:
-    import sys
+# for perlmutter
+if os.path.exist("/global/homes/c/cju33/.conda/envs/venv/lib/python3.12/site-packages"):
     sys.path.append("/global/homes/c/cju33/.conda/envs/venv/lib/python3.12/site-packages")
+if os.path.exist("/global/homes/c/cju33/gym-examples"):
     sys.path.append("/global/homes/c/cju33/gym-examples")
 import multiprocessing as mp
 
