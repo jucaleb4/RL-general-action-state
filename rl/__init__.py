ed88c0d80e00139011c448351eeddf46b207f6a9
Caleb GT <calebju4@gatech.edu>
2024-03-19 16:42:51 -0400

    update init to avoid circular import

--- rl/__init__.py
+++ rl/__init__.py
@@ -2,7 +2,8 @@ from rl.alg import RLAlg
 import rl.utils as utils
 
 from rl.rollout import Rollout
-from rl.estimator import FunctionApproximator
+from rl.estimator import LinearFunctionApproximator
+from rl.estimator import NNFunctionApproximator
 
 from rl.pmd import PMDFiniteStateAction
 from rl.pmd import PMDGeneralStateFiniteAction
