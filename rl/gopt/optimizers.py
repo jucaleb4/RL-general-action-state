fix printing of PMD when there is a large reward and fix gopt optimizer overflow error for the first iteration when the initial points are too close

rl/gopt/optimizers.py
@@ -213,7 +213,8 @@ class ACFastGradDescent(Optimizer):
         :param tol: tolerance for non-convexity/linearization
         """
         if first_iter:
-            return la.norm(next_grad - self._grad)/la.norm(next_x-self._x)
+            # need to add a small value below in case too close ...
+            return la.norm(next_grad - self._grad)/(1e-6 + la.norm(next_x-self._x))
 
         linearization_diff = self._f - next_f - np.dot(next_grad, self._x-next_x)
         if linearization_diff > 0:

rl/pmd.py
@@ -220,9 +220,15 @@ class FOPO(RLAlg):
 
         self.msg += "rollout/\n"
         self.msg += f"  {'itr_ep_len_mean':<{l}}: {np.mean(len_arr_trunc):.2f}\n"
-        self.msg += f"  {'itr_ep_rwd_mean':<{l}}: {np.mean(rwd_arr_trunc):.2f}\n"
+        if np.abs(np.mean(rwd_arr_trunc)) < 1e4:
+            self.msg += f"  {'itr_ep_rwd_mean':<{l}}: {np.mean(rwd_arr_trunc):.2f}\n"
+        else:
+            self.msg += f"  {'itr_ep_rwd_mean':<{l}}: {np.mean(rwd_arr_trunc):.2e}\n"
         self.msg += f"  {'itr_n_ep':<{l}}: {int(self.n_episodes-self.last_iter_ep)}\n"
-        self.msg += f"  {'ep_rwd_25-ma':<{l}}: {moving_avg:.2f}\n"
+        if np.abs(moving_avg) < 1e4:
+            self.msg += f"  {'ep_rwd_25-ma':<{l}}: {moving_avg:.2f}\n"
+        else:
+            self.msg += f"  {'ep_rwd_25-ma':<{l}}: {moving_avg:.2e}\n"
 
         self.msg += "time/\n"
         self.msg += f"  {'itr':<{l}}: {self.t}\n"